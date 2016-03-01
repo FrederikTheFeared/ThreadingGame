1188c2eb76f34b56bbc65ebe94d4ebc1a441d884
Dan Grønnebæk <hirpwastooshort@gmail.com>
2016-03-01 10:10:23 +0100

    Added basic classes

---
+++ IdleGame/IdleGame/GoldMine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleGame
+{
+    class GoldMine : GameObject
+    {
+        public GoldMine(string imagePath, Vector2D startPosition) : base(imagePath, startPosition)
+        {
+
+        }
+    }
+}
---
+++ IdleGame/IdleGame/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleGame
+{
+    class Worker : GameObject
+    {
+        public Worker(string imagePath, Vector2D startPosition) : base (imagePath, startPosition)
+        {
+
+        }
+    }
+}
